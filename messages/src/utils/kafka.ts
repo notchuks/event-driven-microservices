8ed78cbc3c0d936bc8a19ed5372177759a880398
notchuks <chukaezem@gmail.com>
2024-05-28 13:17:19 +0100

    configure kafka

---
+++ messages/src/utils/kafka.ts
@@ -0,0 +1,30 @@
+import { Kafka } from "kafkajs";
+
+const brokers = ["0.0.0.0:9092"];
+
+const kafka = new Kafka({
+    clientId: 'messages-app',
+    brokers,
+});
+
+const producer = kafka.producer();
+
+export async function connectToKafka() {
+    await producer.connect();
+};
+
+export async function disconnectFromKafka() {
+    await producer.disconnect();
+};
+
+const topics = ["message-created"] as const;
+
+export async function sendMessage(topic: typeof topics[number], message: any) {
+
+    return producer.send(({
+        topic,
+        messages: [
+            { value: message }
+        ]
+    }))
+};
